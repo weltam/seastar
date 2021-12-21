c8c1265ba62e1c2ba38750b21a203f9f95d673c1
Welly Tambunan <if05041@gmail.com>
2021-12-21 06:41:43 +0000

    adding make files

--- graviton2.sh
+++ graviton2.sh
@@ -6,4 +6,6 @@ sudo su
 
 pip3 install meson ninja pyelftools cmake
 
+sudo yum install git perf gcc10 gcc10-c++ numactl-devel openssl-devel -y
+
 export PATH=$PATH:/usr/local/bin
--- install-vcpkg.sh
+++ install-vcpkg.sh
@@ -1,10 +1,13 @@
 git clone https://github.com/Microsoft/vcpkg.git
 ./vcpkg/bootstrap-vcpkg.sh
+
 export VCPKG_FORCE_SYSTEM_BINARIES=arm
+export PATH=$PATH:/usr/local/bin
 
 ./vcpkg install boost
 ./vcpkg install c-ares
 ./vcpkg install fmt
 ./vcpkg install numactl
 ./vcpkg install hwloc
+./vcpkg install boost-filesystem
 # cmake .. -GNinja -DCMAKE_TOOLCHAIN_FILE=/home/ec2-user/seastar/vcpkg/scripts/buildsystems/vcpkg.cmake
